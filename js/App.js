d7d15eebb512792e3064006ea04f032c8c41356c
Jackie Munroe <wickedbeauty@users.noreply.github.com>
2015-12-12 16:14:11 -0800

    i got it working

--- js/App.js
+++ js/App.js
@@ -1,98 +1,92 @@
 import React, {Component} from 'react';
-import Message from './message';
+import Message from './Message';
 
 export default class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-    this.state = {
-      history: [],
-      user: '',
-      input: ''
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleKeyDown = this.handleKeyDown.bind(this);
+        this.state = {
+            history: [],
+            user: '',
+            input: ''
+        };
+    }
 
-  componentWillMount() {
-    console.log('componentWillMount');
-  }
+    componentWillMount() {
+        console.log('componentWillMount');
+    }
 
-  componentDidMount() {
-    console.log('componentDidMount');
-    var user = prompt('What is your name?')
-    this.setState({user});
-  }
+    componentDidMount() {
+        console.log('componentDidMount');
+        var user = prompt('What is your name?');
+        this.setState({user});
+    }
 
-  componentWillReceiveProps(nextProps) {
-    console.log('componentWillReceiveProps', nextProps);
-  }
+    componentWillReceiveProps(nextProps) {
+        console.log('componentWillReceiveProps', nextProps);
+    }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('shouldComponentUpdate', nextProps, nextState);
-    return true;
-  }
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('shouldComponentUpdate', nextProps, nextState);
+        return true;
+    }
 
-  componentWillUpdate(nextProps, nextState) {
-    console.log('componentWillUpdate', nextProps, nextState);
-  }
+    componentWillUpdate(nextProps, nextState) {
+        console.log('componentWillUpdate', nextProps, nextState);
+    }
 
-  componentDidUpdate(prevProps, prevState) {
-    console.log('componentDidUpdate', prevProps, prevState);
-  }
+    componentDidUpdate(prevProps, prevState) {
+        console.log('componentDidUpdate', prevProps, prevState);
+    }
 
-  componentWillUnmount() {
-    console.log('componentWillUnmount');
-  }
+    componentWillUnmount() {
+        console.log('componentWillUnmount');
+    }
 
-  render(){
+	render(){
 
-    let history = this.state.history.map((message, index) => (
-        <Message key={index} {...message} />
-    ));
+        let history = this.state.history.map((message, index) => (
+            <Message key={index} {...message} />
+        ));
 
-    return (
-        <div>
-      <ul>{history}</ul>
-        <input
-          id="input"
-          name="message"
-          type="text"
-          onChange={this.handleChange}
-          onKeyDown={this.handleKeyDown}
-          value={this.state.input}
-        />
-      <button
-          id="submit"
-          name="submit"
-          type="submit"
-          onClick={this.handleClick}
-          vale = "submit"
-          />
-      </div>
-    );
+        return (
+            <div className="container">
+            <ul id="list">
+                {history}
+            </ul>
+            <input
+                id="input"
+                type="text"
+                value={this.state.input}
+                onChange={this.handleChange}
+                onKeyDown={this.handleKeyDown}
+            />
+            </div>
+        );
   }
 
   handleChange(event) {
-    this.setState({input: event.target.value});
+	this.setState({input: event.target.value});
   }
 
   handleKeyDown(event) {
-      if(event.key === 'Enter'){
-          event.preventDefault();
-          if(this.state.input != ''){
-              let time = new Date();
+	  if(event.key === 'Enter'){
+		  event.preventDefault();
+		  if(this.state.input != ''){
+			  let time = new Date();
 
-              this.setState({
-                  history: this.state.history.concat({
-                      name: this.state.user,
-                      message: this.state.input,
-                      time: `${time.getHours()}:${time.getMinutes()}`
-                  }),
-                  input: ''
-              });
-          }
-      }
+			  this.setState({
+				  history: this.state.history.concat({
+					  name: this.state.user,
+					  message: this.state.input,
+					  time: `${time.getHours()}:${time.getMinutes()}`
+				  }),
+				  input: ''
+			  });
+		  }
+	  }
   }
 
 }

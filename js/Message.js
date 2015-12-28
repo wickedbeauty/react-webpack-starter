2c6852f15a08824cdf1423d1c7f6d7c7d9ab6881
Jackie Munroe <wickedbeauty@users.noreply.github.com>
2015-12-27 18:09:40 -0800

    url display working

--- js/Message.js
+++ js/Message.js
@@ -5,6 +5,11 @@ import React, {Component} from 'react';
 
 export default class Message extends Component {
 
+    constructor(props){
+        super(props);
+        this.formatMessage = this.formatMessage.bind(this);
+    }
+
     render(){
         let {
             name,
@@ -14,9 +19,16 @@ export default class Message extends Component {
         return (
             <li className="item">
                 <span className="name">{name}: </span>
-                <span className="message">{message}</span>
+                <span className="message" dangerouslySetInnerHTML={{__html: this.formatMessage(message)}}></span>
                 <span className="time">{time}</span>
             </li>
         );
+    }
+
+    formatMessage(text){
+        const URL_PATTERN = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/gi;
+        return  text.replace(URL_PATTERN, function(match){
+            return '<a href="'+match+'" target="_blank">'+match+'</a>';
+        });
     }
 }
